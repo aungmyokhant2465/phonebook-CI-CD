71840eed776469663dd50e2ca99041f8902ffd81
Aung Myo Khant <aungmyokhant2465@gmail.com>
2021-09-10 14:51:48 +0630

    uibuild

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const { json } = require('express')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('body', function getId (req) {
     return JSON.stringify(req.body)
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'))
+
 
 let persons = [
     { 
